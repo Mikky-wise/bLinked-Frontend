bd299fc01b0d971b59af3265bdc7645e5c151f44
Тимур Бедухин <tymur.biedukhin@haiku.dev>
2021-10-07 14:33:56 +0300

    Structure refactoring, fonts introducing, image specific import replaced default

---
+++ src/routes/navigation.js
@@ -0,0 +1,14 @@
+export const PublicRoutes = {
+    AUTH: '/auth/',
+    SIGN_IN: '/auth/sign_in',
+    SIGN_UP1: '/auth/sign_up1',
+    SIGN_UP2: '/auth/sign_up2',
+    FORGOT_PASSWORD: '/auth/forgot_password',
+    OTP: '/auth/otp',
+    RESET_PASSWORD: '/auth/reset',
+    PASSWORD_SUCCESS: '/auth/password_success',
+}
+
+export const PrivateRoutes = {
+    DASHBOARD: '/',
+}
