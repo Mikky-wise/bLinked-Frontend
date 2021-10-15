d93f470d6d3a4bfd739bafc625033956862b41f9
Elia El Atram <mikkybrasi@gmail.com>
2021-10-15 22:41:12 +0200

    Fix routes + clean unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Auth SCSS Start
@@ -17,7 +17,6 @@ import "./assets/scss/dashboard/_settings.scss";
 import "./assets/scss/dashboard/_orders.scss";
 // Dashboard SCSS End
 
-import Dashboard from "./Components/Dashboard";
 import SignInPage from "./Pages/Sign-in/SignIn";
 import SignUpPage1 from "./Pages/Sign-up/SignUp-1";
 import SignUpPage2 from "./Pages/Sign-up/SignUp-2";
@@ -25,6 +24,11 @@ import Forgot from "./Pages/Forgot/Forgot";
 import OTPScreen from "./Pages/Forgot/OTP";
 import CreatePassword from "./Pages/Password/CreatePassword";
 import PasswordReset from "./Pages/Password/PasswordReset";
+import OrdersPage from "./Pages/Orders/Orders";
+import SettingsPage from "./Pages/Settings/SettingsScreen";
+import Header from "./Components/Header";
+import HomePage from "./Pages/Home/HomePage";
+import Sidebar from "./Components/Sidebar";
 
 const PrivateRoute = ({ children, ...rest }) => {
   return (
@@ -42,28 +46,53 @@ const PrivateRoute = ({ children, ...rest }) => {
 };
 
 const App = () => {
+
+  const [activeMenu, setActiveMenu] = useState("");
+  const [activeSidebar, setActiveSidebar] = useState(false);
+
+  const handleMenu = (menuItem) => {
+    setActiveMenu(menuItem);
+  };
+
+  const handleSideBar = () => setActiveSidebar(!activeSidebar);
+
   return (
     <div>
       <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path="/" component={Dashboard} />
-          <Route exact path="/auth/sign_in" component={SignInPage} />
-          <Route exact path="/auth/sign_up1" component={SignUpPage1} />
-          <Route exact path="/auth/sign_up2" component={SignUpPage2} />
-          <Route
-            exact
-            path="/auth/forgot_password"
-            component={Forgot}
-          />
-          <Route exact path="/auth/otp" component={OTPScreen} />
-          <Route exact path="/auth/reset" component={CreatePassword} />
-          <Route
-            exact
-            path="/auth/password_success"
-            component={PasswordReset}
-          />
-          <Route path="*" to={SignInPage} />
-        </Switch>
+        <div className="dashboard-main">
+          <div className="d-flex justify-content-end h-100">
+            <Sidebar
+              handleMenu={handleMenu}
+              activeMenu={activeMenu}
+              activeSidebar={activeSidebar}
+              handleSideBar={handleSideBar}
+            />
+            <div className="dashboard-container pb-4">
+              <Header handleSideBar={handleSideBar} />
+              <Switch>
+                <PrivateRoute exact path="/" component={HomePage} />
+                <Route exact path="/auth/sign_in" component={SignInPage} />
+                <Route exact path="/auth/sign_up1" component={SignUpPage1} />
+                <Route exact path="/auth/sign_up2" component={SignUpPage2} />
+                <Route exact path="/orders" component={OrdersPage} />
+                <Route exact path="/settings" component={SettingsPage} />
+                <Route
+                  exact
+                  path="/auth/forgot_password"
+                  component={Forgot}
+                />
+                <Route exact path="/auth/otp" component={OTPScreen} />
+                <Route exact path="/auth/reset" component={CreatePassword} />
+                <Route
+                  exact
+                  path="/auth/password_success"
+                  component={PasswordReset}
+                />
+                <Route exact path="*" to={SignInPage} />
+              </Switch>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
