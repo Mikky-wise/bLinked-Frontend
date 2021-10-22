b913ae261f28f420c55e36dfea1eb4c3b472b0de
mikkybrasi <mikkybrasi@gmail.com>
2021-10-22 16:26:45 +0200

    Revert "Rename + update file structure"
    This reverts commit 6e2fe135f8a3e9dab615e336b2ddeb7082dd429b.

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { AuthPage } from './module/auth';
+import { PublicRoutes, PrivateRoutes } from './routes/navigation';
+
+// Auth SCSS Start
+import "./assets/scss/auth/_footer.scss";
+import "./assets/scss/auth/_input_box.scss";
+import "./assets/scss/auth/_signin.scss";
+import "./assets/scss/auth/_signup.scss";
+import "./assets/scss/auth/_forgot.scss";
+import "./assets/scss/auth/_index.scss";
+// Auth SCSS End
+
+// Dashboard SCSS Start
+import "./assets/scss/dashboard/_index.scss";
+import "./assets/scss/dashboard/_home.scss";
+import "./assets/scss/dashboard/_settings.scss";
+import "./assets/scss/dashboard/_orders.scss";
+// Dashboard SCSS End
+
+import Dashboard from "./module/dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+         <Route path={PublicRoutes.AUTH} component={AuthPage} />
+          <Route path={PrivateRoutes.DASHBOARD} component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/module/auth/setpassword/setpassword.screen.jsx
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { logo, eye } from "../../../assets/img";
+import Footer from "../_components/_footer";
+
+const SetPasswordScreen = () => {
+  const history = useHistory();
+
+  const [pass, setPass] = useState({
+    npass: "",
+    cpass: "",
+  });
+
+  const [passErr, setPassErr] = useState({
+    npassErr: false,
+    cpassErr: false,
+  });
+
+  const [passFocus, setPassFocus] = useState({
+    npass: false,
+    cpass: false,
+  });
+
+  const inputFocus = (name) => {
+    setPassFocus({
+      ...passFocus,
+      [name]: true,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPass({
+      ...pass,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    const { npass, cpass } = pass;
+    if (!npass || !cpass) {
+      setPassErr({
+        npassErr: !npass ? true : false,
+        cpassErr: !cpass ? true : false,
+      });
+    } else {
+      history.push("/auth/password_success");
+    }
+  };
+
+  return (
+    <div className="auth-main">
+      <div className="container auth-cmn-main">
+        <img src={logo} alt="Logo" className="img-fluid my-5" />
+        <div className="auth-cmn-subcontainer px-md-5 py-5">
+          <div className="auth-cmn-title">Set a new password, 🔐</div>
+          <div className="row justify-content-center">
+            <div className="auth-cmn-subtitle px-5">
+              Kindly enter and set up a new password for your account.
+            </div>
+          </div>
+
+          <div className="row mt-5">
+            <div className="col-lg-12 px-4 mt-md-4 mt-4 auth-input-container fotgot-pass-border">
+              <div
+                className={
+                  passFocus.npass
+                    ? "input-box active w-100"
+                    : passErr.npassErr
+                    ? "input-box w-100 forgot-email-border"
+                    : "input-box w-100"
+                }
+              >
+                <div>
+                  <img src={eye} alt="Eye" className="img-fluid" />
+                </div>
+                <label>New password</label>
+                <input
+                  type="password"
+                  className="w-100"
+                  name="npass"
+                  value={pass.npass}
+                  onFocus={() => inputFocus("npass")}
+                  onChange={handleChange}
+                  onBlur={() => {
+                    if (!pass.npass) {
+                      setPassFocus({
+                        ...passFocus,
+                        npass: false,
+                      });
+                    }
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col-lg-12 px-4 mt-md-4 mt-4 auth-input-container fotgot-pass-border">
+              <div
+                className={
+                  passFocus.cpass
+                    ? "input-box active w-100"
+                    : passErr.cpassErr
+                    ? "input-box w-100 forgot-email-border"
+                    : "input-box w-100"
+                }
+              >
+                <div>
+                  <img src={eye} alt="Eye" className="img-fluid" />
+                </div>
+                <label>Confirm new password</label>
+                <input
+                  type="password"
+                  className="w-100"
+                  name="cpass"
+                  value={pass.cpass}
+                  onFocus={() => inputFocus("cpass")}
+                  onChange={handleChange}
+                  onBlur={() => {
+                    if (!pass.cpass) {
+                      setPassFocus({
+                        ...passFocus,
+                        cpass: false,
+                      });
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="px-2 auth-cmn-btn mt-5">
+            <button className="w-100" onClick={handleClick}>
+              Change password
+            </button>
+          </div>
+
+          <div className="auth-cmn-signin mt-4">
+            Take me back to<span> Login</span>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default SetPasswordScreen;
